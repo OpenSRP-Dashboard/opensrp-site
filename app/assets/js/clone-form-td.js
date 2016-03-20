4f8e28f15d9babe5ed32cb24fdbd5cebc56d8a8a
Proshanto <proshanto@mpower-social.com>
2016-03-20 17:54:14 -0200

    Working onDynamic Schedule rule in dashborad

--- app/assets/js/clone-form-td.js
+++ app/assets/js/clone-form-td.js
@@ -1,62 +1,74 @@
 
 $(function () {
     $('#btnAdd').click(function () {
-        var num     = $('.clonedInput').length, // how many "duplicatable" input fields we currently have
-        newNum  = new Number(num + 1);     // the numeric ID of the new input field being added
-		console.log(1222)
-        $("#rule").append('<div id="entry'+newNum+'" class="clonedInput">'+
-		'<h3 id="reference'+newNum+'" name="reference" class="heading-reference">Rule #'+newNum+'</h3>'+		
-        '<lable>Start Form Name:</label><input type="text" name="startFormName[]" />'+
-         '<lable>End Form Name:</label><input type="text" name="endFormName[]" />'+
-         '<p>Defination</p>'+
-		'<div id="defination_space_'+newNum+'"></div>'+
-		'<div style="clear:both;padding-top:25px;"></div>'+        
-		'<input type="button" class="btnRule" id="btnRule_'+newNum+'" value="add defination">'+
-		'</div>'
-		); 
-   
-    //
+        var num     = $('.clonedInput').length;// how many "duplicatable" input fields we currently have      
+           // the numeric ID of the new input field being added       
+		    var numOfEntry = $('.clonedInput');
+        var newEntryCreateCondition ;
+        
+        for(var i=0;i<num;i++){
+          var  newNum  ;
+          /**
+          if delete a rule from middle & again want to create a rule then deleted number rule must be created
+          unless number ordering missaligned.
+          below loop ensure number ordering.
+        */
+          for(var j=0;j<num;j++){
+            var entry = numOfEntry[j].id;          
+            var splitEntry = entry.split("rule");            
+            if (splitEntry[1] == i+1) {
+              newEntryCreateCondition = true;
+              break;
+            }else{             
+              newEntryCreateCondition = false;
+              continue;
+            }
+          }
+          
+          if (newEntryCreateCondition == true) {
+            newNum  = new Number(num + 1);
+          }else{            
+            newNum= i + 1;
+            break;
+          }
+        }      
+        
+        $("#rule").append('<div id="rule'+newNum+'" class="clonedInput">'+
+          '<h3 id="reference'+newNum+'" name="reference" class="heading-reference">Rule #'+newNum+'</h3>'+		
+          '<lable>Start Form Name:</label><input type="text" name="startFormName_'+newNum+'[]" />'+
+          '<lable>End Form Name:</label><input type="text" name="endFormName'+newNum+'[]" />'+
+          '<a href="#" class="ruleDelete" id="ruleDel'+newNum+'"> Delete</a>'+
+          '<p>Defination</p>'+
+          '<div id="defination_space_'+newNum+'"></div>'+
+          '<div style="clear:both;padding-top:25px;"></div>'+        
+          '<input type="button" class="btnRule" id="btnRule_'+newNum+'" value="add defination">'+
+        '</div>'
+        ); 
     });
 
-	
-	
-	////////Table section //////////
-	
-	
 	$(document).on('click','.btnRule',function(){	    
-		var id = $(this).attr('id').split("_")[1];
-		var chair_qty = $("#chair_qty_"+id).val();
-        var type = $("#type"+id).val();        		     
-		$("#defination_space_"+id).append('<div class="defination" style="border:1px solid #fff;margin-right:5px;;margin-bottom:20px"><lable> Name:</label><input type="text" name="name[]"/>'+
-            '<lable>Value:</label> <input type="text" name="value[]"/><a href="#" class="delete" id="def'+id+'"> Delete</a> </div>'
+		var id = $(this).attr('id').split("_")[1];		       		     
+		$("#defination_space_"+id).append('<div class="defination" style="border:1px solid #fff;margin-right:5px;;margin-bottom:20px"><lable> Name:</label><input type="text" name="name'+id+'[]"/>'+
+            '<lable>Value:</label> <input type="text" name="value'+id+'[]"/><a href="#" class="delete" id="def'+id+'"> Delete</a> </div>'
 			);
-        
-        
 		
 	})
 });
 
-
+$(document).ready(function () {	
+    $(document.body).on("click",'.ruleDelete', function (event) {	
+	event.preventDefault();
+    console.log(this)
+    $(this).parent().parent().parent('.clonedInput').remove();	
+    });
+	
+});
 
 $(document).ready(function () {	
     $(document.body).on("click",'.delete', function (event) {	
 	event.preventDefault();
     console.log(this)
     $(this).parent().parent().parent('.defination').remove();	
-    });
-	
-});
-
-$(document).ready(function () {	
-    $(document.body).on("click",'.submit', function (event) {	
-	event.preventDefault();   
-    var myForm = document.forms.rule_def;
-    var myControls = myForm.elements['startFormName[]'];
-    console.log(myControls.length)
-    for (var i = 0; i < myControls.length; i++) {
-        var aControl = myControls[i].value;
-        console.log(aControl);
-    }
     });
 	
 });
--- app/scripts/controllers/schedule_rule.js
+++ app/scripts/controllers/schedule_rule.js
@@ -14,18 +14,40 @@ angular.module('opensrpSiteApp')
       'AngularJS',
       'Karma'
     ];
-    $scope.startFormName = []
-    $scope.endFormName = []
-    $scope.name = []
-    $scope.value = []
+    function person(firstName,lastName,age,eyeColor) {
+      this.firstName = firstName;
+      for(var i =0;i<4;i++){
+      this.rule = function (name) {
+        this.startFormName = name;
+        this.endFormName= name;
+        this.defination = function(){
+          this.name= "pro";
+          this.value = 12;
+        }
+      }
+      }
+    }
+    function defination(){
+      this.name = "pro";
+      this.value = 12;
+    }
      $scope.save = function() {
       var myForm = document.forms.rule_def;
-      var myControls = myForm.elements['startFormName[]'];
-      console.log(document.forms.rule_def)
-      for (var i = 0; i < myControls.length; i++) {
+      
+      var myControls = myForm.elements['name1[]'];
+     var numOfEntry = $('.clonedInput').length;
+     var json = [               
+                 {firstName:"John", lastName:new defination(), age:50, eyeColor:"blue"}
+                 
+                 ];
+     json.push({firstName:"John", lastName:new defination(), age:50, eyeColor:"blue"})
+     var mainJson = {name:"Mamemem",rule:json}
+     console.log(mainJson)
+     /* for (var i = 0; i < myControls.length; i++) {
+        console.log(i)
         var aControl = myControls[i].value;
         console.log(aControl);
-    }
+    }*/
         ScheduleRule.save($scope.formData,$window,Flash);
       };
   });
