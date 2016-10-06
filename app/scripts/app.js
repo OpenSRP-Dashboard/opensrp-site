61fa00fe74af64ddcb8a93df76401a5f650ba166
asifur77 <asifur77@gmail.com>
2016-10-06 14:23:28 +0600

    Worked on forms updated fields csv export

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,11 +14,10 @@ angular
   //.constant('OPENSRP_WEB_BASE_URL', 'http://192.168.21.246:1234/192.168.21.246:9979')
   //ip for jivita server - 192.168.19.90
   //.constant('OPENSRP_WEB_BASE_URL', 'http://27.147.129.50:1234/192.168.19.55:9979')
-  .constant('OPENSRP_WEB_BASE_URL', 'http://192.168.22.158:1337/192.168.22.158:9979')
-  //.constant('OPENSRP_WEB_BASE_URL', 'http://192.168.22.152:1234/192.168.22.152:9979')
+  .constant('OPENSRP_WEB_BASE_URL', 'http://192.168.23.223:1337/192.168.23.223:9979')
   .constant("HH_REGISTER_ENTRY_URL_API",'27.147.129.50:9979/registers/hh?anm-id=')
   .constant("ELCO_REGISTER_ENTRY_URL_API",'27.147.129.50:9979/registers/ec?anm-id=')
-  .constant("COUCHURL",'http://192.168.22.158:1337/192.168.22.158:5984')
+  .constant("COUCHURL",'http://192.168.23.223:1337/192.168.23.223:5984')
   .config(['AclServiceProvider', function (AclServiceProvider) {
     var myConfig = {
       storage: 'sessionStorage',  // localStorage
